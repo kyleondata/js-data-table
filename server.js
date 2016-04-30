a8d168e63f6502dbe5fcbdd22f3e2786d284e4b1
Anusuya Choudhury <anusuyachoudhury@anusuyas-mbp.gateway.pace.com>
2016-04-29 23:55:57 -0700

    Initial commit. Adding/testing angular

---
+++ server.js
@@ -0,0 +1,27 @@
+
+var express = require('express');
+var app = express();
+var exphbs  = require('express-handlebars');
+var hbs = require('hbs');
+
+
+
+// app.engine('handlebars', 
+// 	exphbs({defaultLayout: 'main'}));
+// app.set('view engine', 'handlebars');
+
+
+
+app.get('/', function(req, res){
+	res.render('index')
+});
+
+app.use('/public', express.static('public'));
+// hbs.registerPartials(__dirname + '/views/partials');
+
+
+
+var port = Number(process.env.PORT || 8080);
+app.listen(port, function(){
+	console.log('Hi There! Head over to port 8080 :)')
+});
